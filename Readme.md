9e501fd254a2b4407f4d9a5b1b3aedb9e434872e
Felix Salcher <felix.salcher@gmail.com>
2016-09-18 17:46:26 +0200

    commiting stubs

--- Readme.md
+++ Readme.md
@@ -1,1 +1,10 @@
-# HWI Projekt #
+# Setup #
+
+To get this project up and running you have to install gulp and bower.
+After that you have to run the following commands:
+
+- `npm install`
+- `bower install`
+- `gulp`
+
+After a short moment a browser window with a running development server should be running
--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,22 @@ var gulp = require('gulp');
 var plumber = require('gulp-plumber');
 var clean = require('gulp-clean');
 var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+var minify = require('gulp-clean-css');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var htmlmin = require('gulp-htmlmin');
 var sourcemaps = require('gulp-sourcemaps');
+var runSequence = require('run-sequence');
+var browserSync = require('browser-sync').create();
+var reload = browserSync.reload;
+
+var src = {
+    html: 'app/*.html',
+    scss: 'app/scss/styles.scss',
+    js: 'app/js/*.js'
+};
 
 gulp.task('default', function() {
     console.log('Hello World');
@@ -13,11 +28,65 @@ gulp.task('clean', function() {
         .pipe(clean());
 });
 
-gulp.task('style', function() {
-    return gulp.src('app/sass/style.scss')
+gulp.task('html', function() {
+   return gulp.src(src.html)
+       .pipe(plumber())
+       .pipe(htmlmin({collapseWhitespace: true}))
+       .pipe(gulp.dest('dist/'))
+       .pipe(reload({stream: true}));
+});
+
+gulp.task('styles', function() {
+    return gulp.src([
+        'bower_components/normalize-css/normalize.css',
+        src.scss])
         .pipe(plumber())
         .pipe(sourcemaps.init())
+        .pipe(concat('styles.scss'))
         .pipe(sass())
+        .pipe(autoprefixer({
+            browsers: ['> 1%']
+        }))
+        .pipe(minify())
+        .pipe(rename({suffix: '.min'}))
         .pipe(sourcemaps.write('../map'))
-        .pipe(gulp.dest('dist/css'));
+        .pipe(gulp.dest('dist/css'))
+        .pipe(reload({stream: true}));
+});
+
+gulp.task('scripts', function() {
+   return gulp.src([
+       'bower_components/jquery/dist/jquery.min.js',
+       src.js
+   ])
+       .pipe(plumber())
+       .pipe(sourcemaps.init())
+       .pipe(concat('bundle.js'))
+       .pipe(uglify({preserveComments: 'license'}))
+       .pipe(rename({suffix: '.min'}))
+       .pipe(sourcemaps.write('../map'))
+       .pipe(gulp.dest('dist/js'))
+       .pipe(reload({stream: true}));
+});
+
+gulp.task('browser-sync', function() {
+   return browserSync.init({
+       server: {
+           baseDir: 'dist/'
+       }
+   });
+});
+
+gulp.task('watch', function() {
+    gulp.watch(src.html, ['html']);
+    gulp.watch(src.scss, ['styles']);
+    gulp.watch(src.js, ['scripts']);
+});
+
+gulp.task('build', ['clean'], function(){
+   return runSequence('styles', 'scripts', 'html');
+});
+
+gulp.task('default', ['clean'], function() {
+    return runSequence('styles', 'scripts', 'html', 'browser-sync', 'watch');
 });
